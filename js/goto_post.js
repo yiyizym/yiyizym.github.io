5d75653b76ff335659c5e3dc303765021d989b98
yiyizym <yiyizym@gmail.com>
2016-12-31 15:08:13 +0800

    Site updated: 2016-12-31 15:08:12

--- js/goto_post.js
+++ js/goto_post.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
   $('section.posts-expand').on('click', 'article', function (event) {
     var path = $(this).data('path');
     if (path) {
-      window.location.pathname = path;
+      // window.location.pathname = path;
+      var win = window.open(path,'_blank');
+      win.focus();
     }
   });
 });
